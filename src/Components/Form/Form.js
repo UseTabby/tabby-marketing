d9ca2a4a338f8fdf8e8dd48d947d7ff30ad26420
Daniel <me@danthms.com>
2018-11-05 22:26:27 +0000

    added mailchimp

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import MailchimpSubscribe from "react-mailchimp-subscribe"
 import './Form.scss';
 
 // Icon
 import Icon from '../../Assets/icon.svg';
+
+const url = "https://usetabby.us19.list-manage.com/subscribe/post?u=2854621da172531f2d046ccb8&amp;id=f32b97f51c";
 
 class Form extends Component {
   render() {
@@ -11,7 +14,7 @@ class Form extends Component {
         <h2>Get Notified</h2>
         <p>Tabby is a self-funded solo venture. I work on it when I can but plan to have it ready in 2 months and 5 days.</p>
         <div className='formBox'>
-          Here
+          <MailchimpSubscribe url={url}/>
         </div>
         <span className='footnote'>I also hate spam, you’ll get none from me. Just one email when Tabby is ready.</span>
       </section>
