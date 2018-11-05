57bac7ddbbd5929084d7f8677dcc3e666aeef8d6
Daniel <me@danthms.com>
2018-11-05 19:51:08 +0000

    Initial commit

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Footer.scss';
+
+// Icon
+import Iconset from '../../Assets/icon-set.svg';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <span class='footnote'>Made by <a href='#'>Dan</a></span>
+        <img src={Iconset} />
+      </footer>
+    );
+  }
+}
+
+export default Footer;
